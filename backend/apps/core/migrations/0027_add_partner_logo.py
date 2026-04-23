5be2343ff7b80c2f743f5d4127c7fa587090d6f3
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2026-04-23 10:46:32 +0400

    fix: rename migration to 0027, fix dependency conflict with server

--- backend/apps/core/migrations/0027_add_partner_logo.py
+++ backend/apps/core/migrations/0027_add_partner_logo.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0025_partner_teammember_alter_projectimage_id_and_more'),
+        ('core', '0024_projectimage_serviceimage'),
     ]
 
     operations = [
